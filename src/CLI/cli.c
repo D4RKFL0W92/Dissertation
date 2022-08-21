de9a1bd51ebd72123389354edfffd22dbbede3e5
D4RKFL0W <calum@D4RKFL0W.D4RKFL0W>
2022-08-21 09:57:31 +0100

    Project root fix attempt

--- src/CLI/cli.c
+++ src/CLI/cli.c
@@ -1,11 +1,10 @@
-#include "cli.h"
+#include "../Modules/ELFinfo/elfinfo.h"
 
 #define TEST32 "/home/calum/Test_Files/while32"
 #define TEST64 "/home/calum/Test_Files/while64"
 
 int main(int argc, char const *argv[])
 {
-    printELFInfo(TEST32, NULL);
-
+    printELFPhdrs(TEST32);
     return 0;
 }
--- src/CLI/cli.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CLI_
-#define _CLI_
-
-#include "../Types/turtle_types.h"
-#include "../Modules/ELFinfo/elfinfo.h"
-// #include "../Modules/Dynamic/elfdynamic.h"
-
-
-#endif
