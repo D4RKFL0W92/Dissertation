dd1b3927887866c13e22134e80171138ec198651
D4RKFL0W <calum@D4RKFL0W.D4RKFL0W>
2022-11-05 12:15:07 +0000

    Test sha1 sum feature

---
+++ tests/sha1_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+from os import listdir
+from os.path import isfile, join
+import os
+
+shasMatch = False
+
+fileDirectory = "./files/"
+logFile = "./sha1-log.txt"
+
+files = [f for f in listdir(fileDirectory) if isfile(join(fileDirectory, f))]
+os.system("rm ./sha1-log.txt")
+
+for f in files:
+	path = fileDirectory + str(f)
+	os.system("../BUILD/turtle_scan -sha1 " + path + " >> " + logFile)
+	a = os.system("sha1sum " + path + " >> " + logFile)
+
+# Read the file line by line, (for some reason sha1sum also outputs the filename,
+# this will have to be stripped before comparison).
+with open(logFile) as file:
+	hasNext = True
+	trueSha = ""
+	mySha = ""
+	while hasNext:
+		line = file.readline()
+		if not line:
+			hasNext = False
+			break
+		else:
+			mySha = line.split()[0]
+
+		# Grab the true sha for comparison
+		line = file.readline()
+		if not line:
+			hasNext = False
+			break
+		else:
+			trueSha = line.split()[0]
+
+		#compare the two hashes
+		if mySha != trueSha:
+			shasMatch = False
+			print("Hashes don't match!\n" + "Turtle Sha: " + mySha + "\nSha Sum: " + trueSha)
+		else:
+			shasMatch = True
+			
+
+if shasMatch:
+	print("All SHA Values Match")
